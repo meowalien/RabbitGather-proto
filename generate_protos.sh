cbdd8d4aadb3041c42782cb74839dd38cb52861e
jacky_li <jacky_li@Jackys-MacBook-Pro.local>
2023-08-10 22:31:35 +0800

    add interest.proto

---
+++ generate_protos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROTO_DIR="./source"  # This is your root directory containing .proto files.
+OUTPUT_DIR="./proto"    # This is where you want your generated files to reside.
+
+# Iterate over each .proto file in the directory.
+find $PROTO_DIR -name "*.proto" | while read -r proto_file; do
+  # Extract the directory path from the full file path.
+  dir=$(dirname "$proto_file")
+  # Use protoc to generate the files, maintaining the directory structure.
+  protoc -I="$PROTO_DIR" --go_out="$OUTPUT_DIR" --go_opt=paths=source_relative --go-grpc_out="$OUTPUT_DIR" --go-grpc_opt=paths=source_relative "$proto_file"
+done
